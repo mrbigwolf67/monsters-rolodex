8a1a99058703a2f6f98b84a095b0ba575cc80d42
mrbigwolf67 <uffec@comhem.se>
2020-08-07 17:39:27 +0200

    Updates

---
+++ precache-manifest.366881f6868e41d39344cb0b41537872.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af3562394cd7a2225e45a40f24ecbd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d756b4c5d704fca4e72",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d756b4c5d704fca4e72",
+    "url": "/static/js/main.fbf9a022.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
